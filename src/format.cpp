80ec994a033e46772ff5de7c950217cb01119d4c
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-02-26 11:00:55 +0100

    Time output formatting

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,27 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds)
+{
+  long HH, MM, SS;
+  SS = seconds % 60;
+  MM = (seconds / 60) % 60;
+  HH = seconds / 3600;
+        
+  std::stringstream HHMMSSstream;
+  
+  HHMMSSstream << std::setw(2) << std::setfill('0') << HH 
+        << ":" << std::setw(2) << std::setfill('0') << MM
+        << ":" << std::setw(2) << std::setfill('0') << SS;
+        
+  return HHMMSSstream.str();
+}
